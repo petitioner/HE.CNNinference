83566e7102ab8285f9af6fa1fbdd0ecdaf67e2ca
John C. Smith <51513317+petitioner@users.noreply.github.com>
2024-12-06 01:36:50 +0800

    Update Methods.h

--- HE.CNNinference/src/Methods.h
+++ HE.CNNinference/src/Methods.h
@@ -1,8 +1,10 @@
 #include "Tools.h"
 
-
 #include <assert.h>
 
+#include <numeric> // for std::accumulate
+
+double computeAverage(const vector<vector<double>>& matrix);
 
 int CNNinference(int rownum, int vctslots, int image_height, int image_width, vector<vector<vector<double>>> dataset, vector<vector<double>> weights);
 
