84d1f60993632fa94dca3ef4dca277da16f74315
John C. Smith <51513317+petitioner@users.noreply.github.com>
2024-12-05 22:46:47 +0800

    Update Methods.cpp

--- HE.CNNinference/src/Methods.cpp
+++ HE.CNNinference/src/Methods.cpp
@@ -28,6 +28,11 @@
 
 int CNNinference(int rownum, int vctslots, int image_height, int image_width, vector<vector<vector<double>>> dataset, vector<vector<double>> weights)
 {
+  cout << "rownum: " << rownum << endl;
+  cout << "vctslots: " << vctslots << endl;
+  cout << "image_height: " << image_height << endl;
+  cout << "image_width: " << image_width << endl;
+
   
  cout << endl << "--------------- END ---------------" << endl;
 
